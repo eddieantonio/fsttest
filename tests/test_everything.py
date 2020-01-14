b59efe880019331466a7107592d394f2740d2950
Eddie Antonio Santos <easantos@ualberta.ca>
2020-01-13 18:34:43 -0700

    Test that cases can fail!

--- tests/test_everything.py
+++ tests/test_everything.py
@@ -15,13 +15,23 @@ from fsttest import execute_test_case  # type: ignore
 def test_transduce_upper_to_lower(a_b_transducer_path: Path):
     test_case = {"upper": "a", "expect": "b"}
     results = execute_test_case(a_b_transducer_path, test_case)
-    assert results
+    assert results.n_passed == 1
+    assert results.n_total == 1
 
 
 def test_transduce_lower_to_upper(a_b_transducer_path: Path):
     test_case = {"lower": "b", "expect": "a"}
     results = execute_test_case(a_b_transducer_path, test_case)
-    assert results
+    assert results.n_passed == 1
+    assert results.n_total == 1
+
+
+def test_failed_test_case(a_b_transducer_path: Path):
+    test_case = {"upper": "a", "expect": "a"}
+    results = execute_test_case(a_b_transducer_path, test_case)
+    assert results.n_passed == 0
+    assert results.n_failed == 1
+    assert results.n_total == 1
 
 
 @pytest.fixture
