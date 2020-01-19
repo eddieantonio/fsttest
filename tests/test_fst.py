18da2e3c174a397517d45021f4bb3918eede9a8a
Eddie Antonio Santos <easantos@ualberta.ca>
2020-01-19 11:15:46 -0700

    Test that the FST disappears after the command.

--- tests/test_fst.py
+++ tests/test_fst.py
@@ -10,7 +10,8 @@ from pathlib import Path
 from fsttest import FST
 
 
-def test_path_exists_during_command(a_b_transducer_path: Path):
+def test_path_exists_only_during_command(a_b_transducer_path: Path):
     fst_desc = {"fomabin": a_b_transducer_path}
     with FST.load_from_description(fst_desc) as fst:
         assert fst.path.exists()
+    assert not fst.path.exists()
