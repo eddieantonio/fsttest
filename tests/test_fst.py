c9e64c0444befe8f8dcbf520b079ad85b0dc3284
Eddie Antonio Santos <easantos@ualberta.ca>
2020-01-19 11:18:59 -0700

    Test load xfst FSTs.

--- tests/test_fst.py
+++ tests/test_fst.py
@@ -7,11 +7,27 @@ Test using the FST class.
 
 from pathlib import Path
 
+import pytest
+
 from fsttest import FST
 
 
-def test_path_exists_only_during_command(a_b_transducer_path: Path):
+def test_path_exists_only_during_command_fomabin(a_b_transducer_path: Path):
     fst_desc = {"fomabin": a_b_transducer_path}
+    with FST.load_from_description(fst_desc) as fst:
+        assert fst.path.exists()
+    assert not fst.path.exists()
+
+
+@pytest.mark.parametrize(
+    "fst_desc",
+    [{"regex": "Cleanup"}, {"compose": ["TInsertion", "NiTDeletion", "Cleanup"]},],
+)
+def test_path_exists_only_during_command_xfst(fst_desc, rewrite_rules_path: Path):
+    """
+    Test that the FST can be loaded from an XFST script.
+    """
+    fst_desc["eval"] = rewrite_rules_path
     with FST.load_from_description(fst_desc) as fst:
         assert fst.path.exists()
     assert not fst.path.exists()
