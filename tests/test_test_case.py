a250eccf0e31750c1f9cfa3608ec9239fb375660
Eddie Antonio Santos <easantos@ualberta.ca>
2020-01-19 13:45:06 -0700

    Test passed test result.

--- tests/test_test_case.py
+++ tests/test_test_case.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-import pytest
+import pytest  # type: ignore
 
 from fsttest import TestCase as _TestCase
 
@@ -17,9 +17,23 @@ from fsttest import TestCase as _TestCase
     "location", [None, "test_verbs.toml"],
 )
 def test_create_test_case(
-    raw_test_case, fst_input: str, expected: str, direction: str, location: str
+    raw_test_case, fst_input: str, expected: str, direction: str, location
 ):
     t = _TestCase.from_description(raw_test_case, location=location)
     assert t.input == fst_input
     assert t.expected == expected
     assert t.direction == direction
+    assert t.location == location
+
+
+@pytest.mark.parametrize(
+    "raw_test_case,location",
+    [
+        ({"upper": "a", "expect": "b"}, "test_verbs.toml"),
+        ({"lower": "b", "expect": "a"}, None),
+    ],
+)
+def test_create_passed_test_from_test_case(raw_test_case, location):
+    t = _TestCase.from_description(raw_test_case, location=location)
+    res = PassedTestResult.from_test_case(t)
+    assert t.location == res.location
