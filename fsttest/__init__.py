853f48bd2c4e8516e670863789b89152e52bceeb
Eddie Antonio Santos <easantos@ualberta.ca>
2020-01-20 10:44:33 -0700

    Rename former eponymous module to _run.py.

--- fsttest/__init__.py
+++ fsttest/__init__.py
@@ -8,9 +8,9 @@ FST test -- test your Foma finite-state transducers!
 from .__version__ import VERSION as __version__
 from ._fst import FST
 from ._results import FailedTestResult, PassedTestResult, TestResults
+from ._run import execute_test_case, run_tests
 from ._test_case import TestCase
 from .exceptions import FSTTestError, TestCaseDefinitionError
-from .fsttest import execute_test_case, run_tests
 
 __all__ = [
     "FST",
