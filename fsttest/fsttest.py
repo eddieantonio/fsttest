d29bef2b9a10fdf43467461e1e029d5a04e763ae
Eddie Antonio Santos <easantos@ualberta.ca>
2020-01-20 10:43:35 -0700

    Move TestCase class to _test_case.py

--- fsttest/fsttest.py
+++ fsttest/fsttest.py
@@ -3,14 +3,14 @@
 
 import sys
 from pathlib import Path
-from typing import Any, Dict, Optional, Union
+from typing import Any, Dict, Optional
 
 import toml
 from blessings import Terminal  # type: ignore
 
 from ._fst import FST
-from ._results import FailedTestResult, PassedTestResult, TestResults
-from .exceptions import TestCaseDefinitionError
+from ._results import TestResults
+from ._test_case import TestCase
 
 # ############################### Constants ################################ #
 
@@ -23,64 +23,6 @@ EX_HAS_FAILED_TEST_CASES = _EX_BASE + 2
 # ################################ Globals ################################# #
 
 term = Terminal()
-
-
-# ################################ Classes ################################# #
-
-
-class TestCase:
-    """
-    An executable test case.
-    """
-
-    def __init__(
-        self, input_: str, expected: str, direction: str, location: Optional[Path]
-    ):
-        self.input = input_
-        self.expected = expected
-        self.direction = direction
-        self.location = location
-
-    def execute(self, fst: FST) -> Union["PassedTestResult", "FailedTestResult"]:
-        transductions = fst.apply([self.input], direction=self.direction)
-        assert (
-            self.input in transductions
-        ), f"Expected to find {self.input} in {transductions}"
-
-        actual_transductions = transductions[self.input]
-
-        if self.expected in actual_transductions:
-            return PassedTestResult(location=self.location)
-        else:
-            return FailedTestResult(
-                given=self.input,
-                expected=self.expected,
-                actual=actual_transductions,
-                location=self.location,
-            )
-
-    @staticmethod
-    def from_description(
-        raw_test_case: Dict[str, Any], location: Optional[Path] = None
-    ) -> "TestCase":
-        """
-        Given a dictionary, parses and returns an executable test case.
-        """
-        # Parse a few things
-        if "expect" not in raw_test_case:
-            raise TestCaseDefinitionError('Missing "expect" in test case')
-        expected = raw_test_case["expect"]
-
-        if "upper" in raw_test_case:
-            direction = "down"
-            fst_input = raw_test_case["upper"]
-        elif "lower" in raw_test_case:
-            direction = "up"
-            fst_input = raw_test_case["lower"]
-        else:
-            raise TestCaseDefinitionError('Missing "upper" or "lower" in test case')
-
-        return TestCase(fst_input, expected, direction, location)
 
 
 # ############################### Functions ################################ #
